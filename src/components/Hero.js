5d01e60d02982f0402e73165c2a4cd543246d4ab
JAY <kgatle280@gmail.com>
2023-08-21 21:30:54 +0200

    updated the feel and look

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -62,52 +62,68 @@ const Hero = () => {
         {/* Section 4 */}
         <section class="container-fluid section-4" id='experience'>
             <div class="row resume-section-content">
-                <h2 class="mb-5 mt-5 primary-heading section-3-heading text-center">Experience</h2>
-                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
-                    <div class="flex-grow-1">
-                        <h3 class="mb-0">Junior Salesforce Developer</h3>
-                        <div class="subheading mb-3">Coronation Fund Managers</div>
-                        <p>Work as part of the development team to code, configure and design software or application
-                            solution on the salesforce platform using Apex, Visualforce, JavaScript and Lightning Web Components. I
-                            work with stakeholders to define system requirements and customize the platform to 
-                             enable users to get the most out of Salesforce technology.<br/>
-                            <b>Skills: Lightning Web Components, Visualforce, Apex, JavaScript, Apex Testing</b>
-                        </p>
+                <h2 class="mb-5 mt-5 primary-heading section-3-heading text-center"> Job Experience</h2>
+                <div class="card mb-5 bg-secondary text-light">
+                    <div class="card-body d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="card-title mb-0">Salesforce Developer</h3>
+                            <div class="card-subtitle mb-1">Coronation Fund Managers</div>
+                            <div class="mb-1"><span class="">January 2022 - Present</span></div>
+                            <p class="card-text">As a developer i gained hands-on experience in customizing Salesforce 
+                                solutions, developing Apex triggers, Visualforce pages, and Lightning components. 
+                                Supported data migration and integration tasks. Followed agile processes to deliver 
+                                scalable solutions. Committed to learning, adaptability, and problem-solving. 
+                                Equipped with a strong foundation for success as a Salesforce developer.<br />
+                                <b>Skills: Lightning Web Components, Visualforce, Apex, JavaScript, Apex Testing</b>
+                            </p>
+                        </div>
                     </div>
-                    {/* <div class="flex-shrink-0"><span class="text-primary">January 2022 - Present</span></div> */}
                 </div>
-                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
-                    <div class="flex-grow-1">
-                        <h3 class="mb-0">Software Developer Intern</h3>
-                        <div class="subheading mb-3">Briskflow</div>
-                        <p>Worked under the guidance of experienced software engineers. Coding and debugging 
-                            software applications and experimenting with design elements to learn as much as i 
-                            can about software engineering.<br/>
-                            <b>Skills: PHP, JavaScript, Web development MVC</b></p>
+
+                <div class="card mb-5 bg-dark text-light">
+                    <div class="card-body d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="card-title mb-0">Software Developer Intern</h3>
+                            <div class="card-subtitle mb-1">Briskflow</div>
+                            <div class="mb-1"><span class="">June 2021 - December 2022</span></div>
+                            <p class="card-text">Contributed to software development projects, collaborating with cross-functional 
+                                teams. Designed, coded, and tested applications using PHP and JavaScript. 
+                                Conducted code reviews, debugged issues, and adhered to Agile/Scrum processes. <br />
+                                <b>Skills: PHP, JavaScript, Web development MVC</b>
+                            </p>
+                        </div>
                     </div>
-                    {/* <div class="flex-shrink-0"><span class="text-primary">June 2021 - December 2022</span></div> */}
                 </div>
                 <h2 class="mb-5 mt-5 primary-heading section-3-heading text-center">Education</h2>
-                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
-                    <div class="flex-grow-1">
-                        <h3 class="mb-0">Honours Science Computer Science</h3>
-                        <div class="subheading mb-3">University of Limpopo</div>
-                        <p>Internet of Things,
-Software Engineering,
-Advanced Databases,
-Computer Networking,
-Computer Security</p>
+                <div class="row">
+                    <div class="col-md-6 mb-4">
+                        <div class="card h-100 p-3 bg-secondary text-light">
+                            <div class="card-body">
+                                <h3 class="card-title">Honours Science Computer Science</h3>
+                                <div class="card-subtitle mb-3">University of Limpopo</div>
+                                <p class="card-text">Internet of Things,
+                                    Software Engineering,
+                                    Advanced Databases,
+                                    Computer Networking,
+                                    Computer Security</p>
+                                <span class="">2021 - 2021</span>
+                            </div>
+                        </div>
                     </div>
-                    <div class="flex-shrink-0"><span class="text-primary">2021 - 2021</span></div>
-                </div>
-                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
-                    <div class="flex-grow-1">
-                        <h3 class="mb-0">Bachelor Science Mathematical Sciences</h3>
-                        <div class="subheading mb-3">University of Limpopo</div>
-                        <p>Mathematical Statistics, Calculus, Algebra, Data Structures, Artificial Intelligence, Applied Mathematics.</p>
+                    <div class="col-md-6 mb-4">
+                        <div class="card h-100 p-3 bg-dark text-light">
+                            <div class="card-body">
+                                <h3 class="card-title">Bachelor Science Mathematical Sciences</h3>
+                                <div class="card-subtitle mb-3">University of Limpopo</div>
+                                <p class="card-text">Mathematical Statistics, Calculus, Algebra, Data Structures, Artificial Intelligence, Applied Mathematics.</p>
+                                <span class="">2018 - 2021</span>
+                            </div>
+                        </div>
                     </div>
-                    <div class="flex-shrink-0"><span class="text-primary">2018 - 2021</span></div>
                 </div>
+
+
+
             </div>
         </section>
     </div>
